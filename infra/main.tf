9a0ed3ecfb9bcb8d213891ffcc832cfd74a5c0e4
polimerakalyan <116016526+polimerakalyan@users.noreply.github.com>
2025-09-21 19:15:53 +0530

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -17,23 +17,19 @@ module "vpc" {
 # Create EKS Cluster
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "20.8.5"
+  version = ">= 19.0"
 
-  cluster_name    = var.cluster_name
+  cluster_name    = "lms-eks"
   cluster_version = "1.29"
-  subnet_ids      = module.vpc.private_subnets
+  subnets         = module.vpc.private_subnets
   vpc_id          = module.vpc.vpc_id
 
-  enable_irsa = true
-
-  node_groups = {
+  eks_managed_node_groups = {
     default = {
-      desired_capacity = var.desired_capacity
-      max_capacity     = 3
-      min_capacity     = 1
-
-      instance_types = [var.instance_type]
-      subnets        = module.vpc.private_subnets
+      desired_size = 2
+      max_size     = 3
+      min_size     = 1
+      instance_types = ["t3.medium"]
     }
   }
 }
