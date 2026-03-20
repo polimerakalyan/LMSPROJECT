6305f2922c29bd3ddbe08e4d8cf009d04e65d09c
polimerakalyan <116016526+polimerakalyan@users.noreply.github.com>
2026-03-20 15:18:22 +0530

    Update models.py

--- learning/models.py
+++ learning/models.py
@@ -86,7 +86,7 @@ class AdminHOD(models.Model):
     id=models.AutoField(primary_key=True)
     admin=models.OneToOneField(CustomUser,on_delete=models.CASCADE)
     created_at=models.DateTimeField(auto_now_add=True)
-    updated_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
     objects=models.Manager()
 
 class Schools(models.Model):
